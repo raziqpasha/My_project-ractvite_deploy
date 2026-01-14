3b83abfa4f8c235237d87fc1389d1ea5d72627a0
raziqpasha <sjraziq@gmail.com>
2026-01-14 14:19:46 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base :"My_project/ractvite_deploy",
+  base :"/My_project/ractvite_deploy/",
   plugins: [react()],
 });
